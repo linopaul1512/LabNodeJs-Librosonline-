6c8fffdf277adfecb96128ef1801badcabfdd5a3
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-09-19 21:12:08 -0400

    Add files via upload

---
+++ src/similiarproducts/entities/Similarproducts.entity.js
@@ -0,0 +1,22 @@
+import { sequelize } from "../../db/db.config.js";
+import { Sequelize } from "sequelize";
+export const SimilarProducts = sequelize.define('similarproducts',
+{
+    SimilarID: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    Name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    Description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    UserID: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
