78612b3f4a959bd9a82b7e8dc16fedeb181d10c5
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-09-19 21:07:08 -0400

    Add files via upload

---
+++ src/routes/loginRouter.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { userController } from '../user/controller.js';
+
+const loginRouter = express.Router();
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Iniciar sesión en el sistema
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login exitoso
+ *       401:
+ *         description: Credenciales inválidas
+ */
+loginRouter.post('/login', userController.login);
+
+export default loginRouter;
