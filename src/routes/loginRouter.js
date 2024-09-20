4d619643b0162e8ff23ec6a203a69c20de984178
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-09-19 21:06:49 -0400

    Delete src/routes/loginRouter.js

--- src/routes/loginRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { userController } from '../user/controller.js';
-
-const loginRouter = express.Router();
-
-loginRouter.post('/login', userController.login);
-
-export default loginRouter;
