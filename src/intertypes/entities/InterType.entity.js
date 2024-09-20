13b533777a4b2cdb19673f5125a7a0e119ac54ea
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-09-19 21:04:27 -0400

    Add files via upload

---
+++ src/intertypes/entities/InterType.entity.js
@@ -0,0 +1,18 @@
+import { sequelize } from "../../db/db.config.js";
+import { Sequelize } from "sequelize";
+export const InterType = sequelize.define('intertype',
+{
+    InterTypeID: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    TypeID: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    PublicationID: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
