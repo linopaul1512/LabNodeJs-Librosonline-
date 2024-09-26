89ea0cf87499ec2b5c34566613baaa887ee813dd
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-09-26 09:01:47 -0400

    Add files via upload

---
+++ src/intersimilar/repository.js
@@ -0,0 +1,38 @@
+import { InterSimilar } from './entities/Intersimilar.entity.js'
+
+
+const createIntersim= async (interX) => {
+    const newInter = await InterSimilar.create(interX);
+    return newInter;
+};
+
+const modifyIntersim = async (id, interX) => {
+    const updInter = await InterSimilar.findOne(
+        {where: { InterTypeID: id }
+    });
+    updInter.PulicationID = interX.PublicationID;
+    updInter.SimilarID = interX.SimilarID;
+    await updInter.save();
+    return updInter;
+}
+
+const deleteIntersim = async (id) => {
+    const delInter = await InterSimilar.findOne(
+        {where: { InterSimilarID: id }
+    });
+    await delInter.deleteIntersim();
+    return delInter;
+};
+
+const filterIntersim = async (id) => {
+    return await InterSimilar.findOne(
+        {where: { InterSimilarID: id }
+    });
+};
+
+export const intersimRepository = {
+    createIntersim,
+    modifyIntersim,
+    deleteIntersim,
+    filterIntersim
+}
