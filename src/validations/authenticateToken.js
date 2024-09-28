3d4c7e53adadbfea2c383f71cbb96a92809d70c3
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-09-27 22:22:06 -0400

    Add files via upload

---
+++ src/validations/authenticateToken.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Token de autenticación no proporcionado' });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Token no válido' });
+        }
+        req.user = { id: user.UserID };
+        next();
+    });
+};
+
+export default authenticateToken;
