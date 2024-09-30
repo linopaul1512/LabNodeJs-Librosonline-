30e7e2240436aaf8a6414faba165affa0b48c1e0
Lino Benavides <linobenavidesgabaldon@gmail.com>
2024-09-30 14:42:56 -0400

    controller author

--- src/author/controller.js
+++ src/author/controller.js
@@ -1,24 +1,55 @@
 import { authorService } from "./service.js"; 
 
 
+// Agregar
 const addAuth = async (req, res) => {
-    res.status(200).json(await authorService.addAuth(req.body));
+    const error = validateAuthorFields(req, res);
+    if (error) return;  
+
+    try {
+        const newAuthor = await authorService.addAuth(req.body);
+        res.status(200).json(newAuthor);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al agregar el autor', error });
+    }
 };
 
+// modificar
 const modifyAuth = async (req, res) => {
-    res.status(200).json(await authorService.modifyAuth(req.params.id, req.body));
+    const error = validateAuthorFields(req, res);
+    if (error) return; 
+    
+    try {
+        const updatedAuthor = await authorService.modifyAuth(req.params.id, req.body);
+        res.status(200).json(updatedAuthor);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al modificar el autor', error });
+    }
 };
 
+//mostar todos los autores
+const showAuth = async (req, res) => {
+    res.status(200).json(await authorService.showAuths());
+};
+
+//eliminar
 const deleteAuth = async (req, res) => {
-    res.status(200).json(await authorService.deleteAuthor(req.params.id));
+    try {
+        const deletedAuthor = await authorService.deleteAuthor(req.params.id);
+        res.status(200).json(deletedAuthor);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar el autor', error });
+    }
 };
 
+//buscar
 const filterAuth = async (req, res) => {
-    res.status(200).json(await authorService.filterAuthor(req.params.id));
-};
-
-const showAuthors = async (req, res) => {
-    res.status(200).json(await authorService.showAuthors());
+    try {
+        const author = await authorService.filterAuthor(req.params.id);
+        res.status(200).json(author);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al filtrar el autor', error });
+    }
 };
 
 export const authorController = {
