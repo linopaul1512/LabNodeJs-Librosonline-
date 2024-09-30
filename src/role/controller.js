cd1607392875a432f809d1344833488369883cdd
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-09-30 16:19:44 -0400

    Add files via upload

---
+++ src/role/controller.js
@@ -0,0 +1,31 @@
+import {roleService} from "./service.js"; 
+import { validateRoleFields } from '../validations/validateFields.js';
+
+const addRole = async (req, res) => {
+    const error = validateRoleFields(req, res);
+    if (error) return;  
+
+    try {
+        const newRole = await roleService.addRole(req.body);
+        res.status(200).json(newRole);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al agregar el role', error });
+    }
+};
+
+const modifyRole = async (req, res) => {
+    const error = validateRoleFields(req, res);
+    if (error) return; 
+    
+    try {
+        const updatedRole = await roleService.modifyRole(req.params.id, req.body);
+        res.status(200).json(updatedRole);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al modificar el role', error });
+    }
+};
+
+export const roleController = {
+    addRole,
+    modifyRole,
+}
