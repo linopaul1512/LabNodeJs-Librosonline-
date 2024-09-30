e20667ec7b68e995a368a434db140f576e10f112
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-09-30 15:57:46 -0400

    Add files via upload

---
+++ src/publication/controller.js
@@ -0,0 +1,56 @@
+import {publicationService} from './service.js';
+import { validatePublicationFields } from '../validations/validateFields.js';
+
+const showPubs = async (req, res) => {
+    res.status(200).json(await publicationService.showPubs(req.params.id));
+};
+
+const filterPub = async (req, res) => {
+    try {
+        const pub = await publicationService.filterPub(req.params.id);
+        res.status(200).json(pub);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al filtrar la puvlicación', error });
+    }
+};
+
+const addPub = async (req, res) => {
+    const error = validatePublicationFields(req, res);
+    if (error) return;  
+
+    try {
+        const newPub = await publicationService.addPub(req.body);
+        res.status(200).json(newPub);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al agregar la publicación', error });
+    }
+};
+
+const modifyPub= async (req, res) => {
+    const error = validatePublicationFields(req, res);
+    if (error) return; 
+    
+    try {
+        const updatedPub = await publicationService.modifyPub(req.params.id, req.body);
+        res.status(200).json(updatedPub);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al modificar la publicación', error });
+    }
+};
+
+const deletePub = async (req, res) => {
+    try {
+        const deletedPub = await publicationService.deletePub(req.params.id);
+        res.status(200).json(deletedPub);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar la publicación', error });
+    }
+};
+
+export const publicationController = {
+    addPub,
+    modifyPub,
+    filterPub,
+    deletePub,
+    showPubs
+}
