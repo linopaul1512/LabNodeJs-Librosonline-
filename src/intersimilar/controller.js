2a6b9f3576281554c5ceefd96089155a014bfefb
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-10-03 00:17:13 -0400

    Add files via upload

---
+++ src/intersimilar/controller.js
@@ -0,0 +1,57 @@
+import {intersimilarService} from './service.js';
+import { validateInterSimilarFields } from "../validations/validateFields.js";
+
+
+//Agregar
+const addIntersim = async (req, res) => {
+    try {
+        const newInter = await intersimilarService.addIntersim(req.body);
+        res.status(200).json(newInter);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al agregar', error });
+    }
+};
+
+// modificar
+const modifyIntersim = async (req, res) => {
+    try {
+        const updatedInter = await intersimilarService.addIntersim(req.params.id, req.body);
+        res.status(200).json(updatedInter);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al modificar', error });
+    }
+};
+
+//mostar todos 
+const showIntersims  = async (req, res) => {
+    res.status(200).json(await intersimilarService.showIntersim());
+};
+
+//eliminar
+const deleteIntersim = async (req, res) => {
+    try {
+        const deletedIntersim = await intersimilarService.deleteIntersim(req.params.id);
+        res.status(200).json(deletedIntersim);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar', error });
+    }
+};
+
+//buscar
+const filterIntersim = async (req, res) => {
+    try {
+        const inter = await intersimilarService.filterIntersim(req.params.id);
+        res.status(200).json(inter);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al filtrar', error });
+    }
+};
+
+
+export const intersimController = {
+    addIntersim,
+    modifyIntersim,
+    filterIntersim,
+    deleteIntersim,
+    showIntersims
+}
