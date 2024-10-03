738d61330f0ad2477289b1367f321e299eb731cf
Minatozaki <130718064+lMinatozaki@users.noreply.github.com>
2024-10-03 00:19:46 -0400

    Add files via upload

---
+++ src/publication/service.js
@@ -0,0 +1,43 @@
+import { publicationRepository } from "./repository.js";
+
+
+const addPub= async (simObj) => {
+  const newPub = await publicationRepository.createPub(simObj);
+  return {
+    publication: newPub
+  };
+};
+
+const modifyPub = async (id, interObj) => {
+  const updSim = await publicationRepository.modifyPub(id, interObj);
+  return { 
+    updatedSim: updSim
+  };
+};
+
+const deletePub = async (id) => {
+  const delPub = await publicationRepository.deletePub(id);
+  return { deletedPub: delPub };
+};
+
+const filterPub = async (id) => {
+  const publication = await publicationRepository.filterPub(id);
+  return {
+    PublicationID: publication.PublicationID,
+    AuthorID:publication.AuthorID,
+    TypeID: publication.TypeID,
+    Name: publication.Name,
+    Date: publication.Date,
+    Description: publication.Description,
+    Content: publication.Content
+  };
+};
+
+
+
+export const publicationService = {
+    addPub,
+    modifyPub,
+    deletePub,
+    filterPub
+}
